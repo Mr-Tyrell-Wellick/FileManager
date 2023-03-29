6e40c1dd3372e5980587a26abe5d987dd7f97138
Mr-Tyrell-Wellick <rorshix97@gmail.com>
2023-03-30 00:38:40 +0400

    Initial Commit

---
+++ FileManager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FileManager
+//
+//  Created by Ульви Пашаев on 30.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
